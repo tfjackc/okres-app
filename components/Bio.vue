67d18359b48d94aeafa7d7111c2c44a117a25e87
tfjackc <colpitt.jack@gmail.com>
2024-05-14 21:49:21 -0700

    adjusting flag

--- components/Bio.vue
+++ components/Bio.vue
@@ -6,7 +6,13 @@
   <div class="w-1/2 flex flex-col items-center justify-center">
     <div class="flex flex row items-center">
         <div class="text-3xl text-center font-bold">Tulsa, Oklahoma</div>
-        <NuxtImg class="px-5" src="tulsa-flag.jpg" height="70px"></NuxtImg>
+        <NuxtImg
+          class="px-5"
+          src="tulsa-flag.jpg"
+          height="70"
+          width="auto"
+          :style="{ height: '70px', width: 'auto' }"
+        />
     </div>
     <div class="text-2xl text-center font-bold text-slate-600 mb-3 font-sans">12+ Years of Industry Experience</div>
     <p class="text-center font-sans text-xl">Graduated in the fall of 2013 from The University of Tulsa with a Bachelor of Science degree in Petroleum Engineering. Paige is intimately familiar with the complexities of the oil and gas industry, being raised in a family that has owned, drilled, and operated wells in Oklahoma for multiple generations. Paige specializes in oil & gas asset evaluations, economic analysis, and reserve forecasting.</p>
